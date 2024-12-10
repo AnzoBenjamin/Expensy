3f241da465d69b59faa820ffc2ae0a24279066e3
AnzoBenjamin <anzobnjmn@gmail.com>
2024-12-10 12:33:35 +0300

    fix: changing the prod server url

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,6 @@
 import svgToDataUri from "mini-svg-data-uri";
 import { default as flattenColorPalette } from "tailwindcss/lib/util/flattenColorPalette";
-const flowbite = require("flowbite-react/tailwind");
-
+import flowbite from 'flowbite-react/tailwind'
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
